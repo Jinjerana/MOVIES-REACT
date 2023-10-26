94914d6ad10f3645fad796756724223a1f8f6ba4
Jinjerana <125341676+Jinjerana@users.noreply.github.com>
2023-10-26 13:24:25 +0200

    18

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,13 +3,12 @@ import { useFetchTrendingMovies } from 'hooks';
 
 const Home = () => {
   const { movies, isLoading, error } = useFetchTrendingMovies();
-  
+
   return (
     <>
-    {isLoading && <p>Loading...</p>}
-    {error && <p>Something went wrong...</p>}
-    {movies.length > 0 && <MoviesList movies={movies} />} 
-      
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something went wrong...</p>}
+      {movies.length > 0 && <MoviesList movies={movies} />}
     </>
   );
 };
